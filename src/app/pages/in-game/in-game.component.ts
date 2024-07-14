d78aed1a80fa83d292aff5e8a2afe3e6796008e0
omar karake <omar.karake@amalitechtraining.org>
2024-07-14 19:39:29 +0200

    implementing: implementing logic for all game logic implementation

--- src/app/pages/in-game/in-game.component.ts
+++ src/app/pages/in-game/in-game.component.ts
@@ -3,13 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-in-game',
   templateUrl: './in-game.component.html',
-  styleUrl: './in-game.component.css'
+  styleUrls: ['./in-game.component.css']
 })
 export class InGameComponent {
   word = 'UNITED KINGDOM';
   specialLetters = ['T', 'E', 'K', 'G', 'M'];
+  disabledLetters = ['D', 'I', 'N', 'O', 'U'];
+  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
   isSpecialLetter(letter: string): boolean {
     return this.specialLetters.includes(letter);
+  }
+
+  isDisabledLetter(letter: string): boolean {
+    return this.disabledLetters.includes(letter);
   }
 }
