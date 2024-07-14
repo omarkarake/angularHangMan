ac078853a36ec16a5b12589b04fe1cc252888e13
omar karake <omar.karake@amalitechtraining.org>
2024-07-14 11:16:03 +0200

    feat: working on how to play button feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,14 @@ module.exports = {
       },
       backgroundImage: {
         'custom-gradient': 'linear-gradient(to bottom, #130556 0%, #000000 100%)',
+        'pink-gradient': 'linear-gradient(to bottom, #FE71FE 0%, #7199FF 100%)',
+      },
+      backgroundColor: {
+        'white-opacity-20': 'rgba(255, 255, 255, 0.2)',
+      },
+      boxShadow: {
+        'inner-custom-bottom-btn': 'inset 0 -2px 0 3px #140E66',
+        'inner-custom-top-btn': 'inset 0 1px 0 6px #3C74FF',
       },
     },
   },
