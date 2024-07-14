7cd8b6d0e4f7c5a209f8a7d9c1263a34cf3f05df
omar karake <omar.karake@amalitechtraining.org>
2024-07-14 09:27:34 +0200

    initial commit, setting up the hangman project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mouseMemoirs: ["MouseMemoirs", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
