30d65fdafb42603c58937a29b2f235feec18b9a8
omar karake <omar.karake@amalitechtraining.org>
2024-07-14 18:49:42 +0200

    implementing: adding displaying words logic

--- src/app/pages/in-game/in-game.component.ts
+++ src/app/pages/in-game/in-game.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './in-game.component.css'
 })
 export class InGameComponent {
+  word = 'UNITED KINGDOM';
+  specialLetters = ['T', 'E', 'K', 'G', 'M'];
 
+  isSpecialLetter(letter: string): boolean {
+    return this.specialLetters.includes(letter);
+  }
 }
