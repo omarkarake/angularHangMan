c27aaed8ba6493a7bdf4c700dc28552ed9208f25
omar karake <omar.karake@amalitechtraining.org>
2024-07-15 20:35:16 +0200

    rendering the categories in pick-category page based on the array provided

--- src/app/pages/pick-category/pick-category.component.ts
+++ src/app/pages/pick-category/pick-category.component.ts
@@ -1,21 +1,25 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+type Categories = {
+  [key: string]: string[];
+};
 @Component({
   selector: 'app-pick-category',
   templateUrl: './pick-category.component.html',
-  styleUrl: './pick-category.component.css',
+  styleUrls: ['./pick-category.component.css'],
 })
 export class PickCategoryComponent {
   @Output() goBack = new EventEmitter();
   @Output() pickCategoryEvent = new EventEmitter();
   @Output() categoryPicked = new EventEmitter();
   @Input('pageType') pageType: string | undefined;
+  @Input() categories: Categories = {};
 
   constructor() {}
 
   backToHome() {
     this.goBack.emit();
   }
+  
   pickCategory(category: string) {
     this.categoryPicked.emit(category);
     this.pickCategoryEvent.emit(category);
