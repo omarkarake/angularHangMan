7930b37ca1c196833b0f9c1e25c829fec6421c18
omar karake <omar.karake@amalitechtraining.org>
2024-07-15 09:39:20 +0200

    ft: added logic for clicking category, before opening game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,51 @@ export class AppComponent {
   showHowToPlay = false;
   showPickACategory = false;
 
+  data = {
+    movies: [
+        "The Shawshank Redemption",
+        "The Godfather",
+        "The Dark Knight",
+        "Pulp Fiction",
+        "Forrest Gump"
+    ],
+    tvShows: [
+        "Breaking Bad",
+        "Game of Thrones",
+        "Friends",
+        "The Office",
+        "Stranger Things"
+    ],
+    countries: [
+        "United States",
+        "Canada",
+        "Germany",
+        "Australia",
+        "Japan"
+    ],
+    capitalCities: [
+        "Washington, D.C.",
+        "Ottawa",
+        "Berlin",
+        "Canberra",
+        "Tokyo"
+    ],
+    animals: [
+        "Lion",
+        "Elephant",
+        "Dolphin",
+        "Penguin",
+        "Kangaroo"
+    ],
+    sports: [
+        "Soccer",
+        "Basketball",
+        "Tennis",
+        "Cricket",
+        "Baseball"
+    ]
+};
+
   constructor() {}
 
   openHowToPlay() {
@@ -29,5 +74,9 @@ export class AppComponent {
     this.showHome = true;
     this.showHowToPlay = false;
     this.showPickACategory = false; // Fix this line
+  }
+
+  pickCategoryEvent(category: string){
+    console.log(category);
   }
 }
--- src/app/pages/pick-category/pick-category.component.ts
+++ src/app/pages/pick-category/pick-category.component.ts
@@ -3,16 +3,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-pick-category',
   templateUrl: './pick-category.component.html',
-  styleUrl: './pick-category.component.css'
+  styleUrl: './pick-category.component.css',
 })
 export class PickCategoryComponent {
   @Output() goBack = new EventEmitter();
+  @Output() pickCategoryEvent = new EventEmitter();
   @Input('pageType') pageType: string | undefined;
-
 
   constructor() {}
 
   backToHome() {
     this.goBack.emit();
+  }
+  pickCategory(category: string) {
+    this.pickCategoryEvent.emit(category);
   }
 }
