9a5c05ad4dbb533156955d38e22196dae715420a
omar karake <omar.karake@amalitechtraining.org>
2024-07-15 11:21:19 +0200

    ft: adding logic for deleting special letter from special letters array when found from keyboard

--- src/app/pages/in-game/in-game.component.ts
+++ src/app/pages/in-game/in-game.component.ts
@@ -3,16 +3,17 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-in-game',
   templateUrl: './in-game.component.html',
-  styleUrls: ['./in-game.component.css']
+  styleUrls: ['./in-game.component.css'],
 })
 export class InGameComponent implements OnInit {
   @Input() word: string = '';
   @Input() specialLetters: string[] = [];
   disabledLetters: string[] = [];
   alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  indexOfSpecialLetter: number | undefined;
   ngOnInit(): void {
-    console.log(this.word, this.disabledLetters);
-    
+    console.log("initial word and disabled letter", this.word, this.disabledLetters);
+    console.log("initial word and special letters",this.word, this.specialLetters);
   }
   isSpecialLetter(letter: string): boolean {
     return this.specialLetters.includes(letter);
@@ -21,13 +22,23 @@ export class InGameComponent implements OnInit {
   isDisabledLetter(letter: string): boolean {
     return this.disabledLetters.includes(letter);
   }
-  
-  isTheLetterIncludeInString(letter: string){
+
+  isTheLetterIncludeInString(letter: string) {
     // this.disabledLetters.push(letter);
     // return this.word.toLowerCase().includes(letter.toLowerCase());
-    if(!this.word.toLowerCase().includes(letter.toLowerCase())){
-      this.disabledLetters.push(letter)
-      console.log(this.disabledLetters);
+    if (!this.word.toLowerCase().includes(letter.toLowerCase())) {
+      this.disabledLetters.push(letter);
+      console.log(letter);
+      console.log('word', this.word);
+      console.log('special letter', this.specialLetters);
+    }else{
+      console.log(letter);
+      console.log('word: ', this.word);
+      console.log('special letter: ', this.specialLetters);
+      console.log('special letter index: ', this.specialLetters.indexOf(letter));
+      this.indexOfSpecialLetter = this.specialLetters.indexOf(letter)
+      this.specialLetters.splice(this.indexOfSpecialLetter, 1)
+      console.log('special letter after deleting index: ', this.specialLetters);
     }
   }
 }
