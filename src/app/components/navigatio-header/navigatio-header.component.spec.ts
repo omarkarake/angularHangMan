e68a6d731bf06de133758d31178c20d5c5281872
omar karake <omar.karake@amalitechtraining.org>
2024-07-21 12:18:27 +0200

    test(navigatio-header): implement the test on should create

--- src/app/components/navigatio-header/navigatio-header.component.spec.ts
+++ src/app/components/navigatio-header/navigatio-header.component.spec.ts
@@ -1,16 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavigatioHeaderComponent } from './navigatio-header.component';
+import { GameStateService } from '../../services/game-state.service';
+import { CustomTransformPipe } from '../../pipe/custom-transform.pipe';
+import { of } from 'rxjs';
 
 describe('NavigatioHeaderComponent', () => {
   let component: NavigatioHeaderComponent;
   let fixture: ComponentFixture<NavigatioHeaderComponent>;
+  let gameStateServiceMock: any;
 
   beforeEach(async () => {
+    // Mocking GameStateService
+    gameStateServiceMock = {
+      losedLetterLength$: of(0),
+      gameResult$: of(null),
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [NavigatioHeaderComponent]
-    })
-    .compileComponents();
+      declarations: [NavigatioHeaderComponent, CustomTransformPipe],
+      providers: [{ provide: GameStateService, useValue: gameStateServiceMock }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavigatioHeaderComponent);
     component = fixture.componentInstance;
